0c465c94c3f168c19ddd418d5e8b053a4b5cfe7f
JoaoPedroFerreira31 <jpmferreira31@gmail.com>
2020-11-26 22:24:38 +0000

    Finish Search ConsultarUsers

--- WindowsFormsApp/ViewUsersForm.Designer.cs
+++ WindowsFormsApp/ViewUsersForm.Designer.cs
@@ -30,12 +30,9 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.textBox_Id = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox_FirstName = new System.Windows.Forms.TextBox();
-            this.btnPesquisarNome = new System.Windows.Forms.Button();
-            this.btnPesquisarId = new System.Windows.Forms.Button();
             this.btnVerTodos = new System.Windows.Forms.Button();
             this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,59 +41,23 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label1.Location = new System.Drawing.Point(161, 22);
+            this.label1.Location = new System.Drawing.Point(173, 24);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.Size = new System.Drawing.Size(94, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Inserir IdUser";
+            this.label1.Text = "Filtrar Pesquisa";
             // 
             // textBox_Id
             // 
-            this.textBox_Id.Location = new System.Drawing.Point(250, 19);
+            this.textBox_Id.Location = new System.Drawing.Point(399, 22);
             this.textBox_Id.Name = "textBox_Id";
-            this.textBox_Id.Size = new System.Drawing.Size(67, 20);
+            this.textBox_Id.Size = new System.Drawing.Size(385, 20);
             this.textBox_Id.TabIndex = 2;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label2.Location = new System.Drawing.Point(422, 22);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(102, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Inserir FirstName";
-            // 
-            // textBox_FirstName
-            // 
-            this.textBox_FirstName.Location = new System.Drawing.Point(530, 18);
-            this.textBox_FirstName.Name = "textBox_FirstName";
-            this.textBox_FirstName.Size = new System.Drawing.Size(173, 20);
-            this.textBox_FirstName.TabIndex = 4;
-            // 
-            // btnPesquisarNome
-            // 
-            this.btnPesquisarNome.Location = new System.Drawing.Point(709, 17);
-            this.btnPesquisarNome.Name = "btnPesquisarNome";
-            this.btnPesquisarNome.Size = new System.Drawing.Size(75, 23);
-            this.btnPesquisarNome.TabIndex = 5;
-            this.btnPesquisarNome.Text = "Pesquisar";
-            this.btnPesquisarNome.UseVisualStyleBackColor = true;
-            // 
-            // btnPesquisarId
-            // 
-            this.btnPesquisarId.Location = new System.Drawing.Point(323, 17);
-            this.btnPesquisarId.Name = "btnPesquisarId";
-            this.btnPesquisarId.Size = new System.Drawing.Size(75, 23);
-            this.btnPesquisarId.TabIndex = 6;
-            this.btnPesquisarId.Text = "Pesquisar";
-            this.btnPesquisarId.UseVisualStyleBackColor = true;
-            this.btnPesquisarId.Click += new System.EventHandler(this.btnPesquisarId_Click);
+            this.textBox_Id.TextChanged += new System.EventHandler(this.textBox_Id_TextChanged);
             // 
             // btnVerTodos
             // 
-            this.btnVerTodos.Location = new System.Drawing.Point(14, 17);
+            this.btnVerTodos.Location = new System.Drawing.Point(14, 19);
             this.btnVerTodos.Name = "btnVerTodos";
             this.btnVerTodos.Size = new System.Drawing.Size(128, 23);
             this.btnVerTodos.TabIndex = 7;
@@ -109,23 +70,33 @@
             this.dataGridView.AllowUserToAddRows = false;
             this.dataGridView.AllowUserToDeleteRows = false;
             this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView.Location = new System.Drawing.Point(14, 67);
+            this.dataGridView.Location = new System.Drawing.Point(14, 58);
             this.dataGridView.Name = "dataGridView";
             this.dataGridView.ReadOnly = true;
-            this.dataGridView.Size = new System.Drawing.Size(770, 361);
+            this.dataGridView.Size = new System.Drawing.Size(770, 380);
             this.dataGridView.TabIndex = 8;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "IdUser",
+            "FirstName",
+            "LastName",
+            "Email"});
+            this.comboBox1.Location = new System.Drawing.Point(272, 21);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 9;
             // 
             // ViewUsersForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.dataGridView);
             this.Controls.Add(this.btnVerTodos);
-            this.Controls.Add(this.btnPesquisarId);
-            this.Controls.Add(this.btnPesquisarNome);
-            this.Controls.Add(this.textBox_FirstName);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox_Id);
             this.Controls.Add(this.label1);
             this.Name = "ViewUsersForm";
@@ -140,11 +111,8 @@
         #endregion
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox_Id;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox_FirstName;
-        private System.Windows.Forms.Button btnPesquisarNome;
-        private System.Windows.Forms.Button btnPesquisarId;
         private System.Windows.Forms.Button btnVerTodos;
         private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- WindowsFormsApp/ViewUsersForm.cs
+++ WindowsFormsApp/ViewUsersForm.cs
@@ -1,8 +1,10 @@
-﻿using PRE.Model.Model;
+﻿using PRE.Data.Repositories;
+using PRE.Model.Model;
 using PRE.Services.Services;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
@@ -15,13 +17,17 @@ namespace WindowsFormsApp
 {
     public partial class ViewUsersForm : Form
     {
-        DataTable table;
+
+
+        //Connection String
+        private static string _cs = ConfigurationManager.ConnectionStrings["ReceitasEstrelaresCS"].ConnectionString;
+        SqlConnection connection = new SqlConnection(_cs);
 
         public ViewUsersForm()
         {
             InitializeComponent();
-        }
-
+        }    
+        
         private void ViewUsersForm_Load(object sender, EventArgs e)
         {
             UserService userService = new UserService();
@@ -30,26 +36,44 @@ namespace WindowsFormsApp
             dataGridView.DataSource= getAll;
         }
 
-        private void btnPesquisarId_Click(object sender, EventArgs e)
-        {
-            UserService userService = new UserService();            
-
-            /*int id = int.Parse(textBox_Id.Text);
-            User getById = userService.GetById(id) ;
-            dataGridView.DataSource = getById;
-            dataGridView.Refresh();*/
-
-            DataTable dt = new DataTable();
-            
-            
-        }
-
         private void btnVerTodos_Click(object sender, EventArgs e)
         {
             UserService userService = new UserService();
 
             List<User> getAll = userService.GetAll();
             dataGridView.DataSource = getAll;
+        }
+            
+        private void textBox_Id_TextChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.Text == "IdUser")
+            {
+                SqlDataAdapter sda = new SqlDataAdapter("SELECT IdUser, FirstName, LastName, Gender, Email, BirthDate, IsAdmin, Blocked FROM Users_tbl WHERE IdUser LIKE '" + textBox_Id.Text + "%'", connection);
+                DataTable dataTable = new DataTable();
+                sda.Fill(dataTable);
+                dataGridView.DataSource = dataTable;
+            }
+            else if (comboBox1.Text == "FirstName")
+            {
+                SqlDataAdapter sda = new SqlDataAdapter("SELECT IdUser, FirstName, LastName, Gender, Email, BirthDate, IsAdmin, Blocked FROM Users_tbl WHERE FirstName LIKE '" + textBox_Id.Text + "%'", connection);
+                DataTable dataTable = new DataTable();
+                sda.Fill(dataTable);
+                dataGridView.DataSource = dataTable;
+            }
+            else if (comboBox1.Text == "LastName")
+            {
+                SqlDataAdapter sda = new SqlDataAdapter("SELECT IdUser, FirstName, LastName, Gender, Email, BirthDate, IsAdmin, Blocked FROM Users_tbl WHERE LastName LIKE '" + textBox_Id.Text + "%'", connection);
+                DataTable dataTable = new DataTable();
+                sda.Fill(dataTable);
+                dataGridView.DataSource = dataTable;
+            }
+            else if (comboBox1.Text == "Email")
+            {
+                SqlDataAdapter sda = new SqlDataAdapter("SELECT IdUser, FirstName, LastName, Gender, Email, BirthDate, IsAdmin, Blocked FROM Users_tbl WHERE Email LIKE '" + textBox_Id.Text + "%'", connection);
+                DataTable dataTable = new DataTable();
+                sda.Fill(dataTable);
+                dataGridView.DataSource = dataTable;
+            }
         }
     }
 }
