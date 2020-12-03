cac11a6b0550339883dc52bb71aaddca78f8ace3
JoaoPedroFerreira31 <jpmferreira31@gmail.com>
2020-12-03 22:23:29 +0000

    Create IngredientService

---
+++ PRE.Services/Services/IngredientService.cs
@@ -0,0 +1,30 @@
+﻿using PRE.Data.Repositories;
+using PRE.Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRE.Services.Services
+{
+    public class IngredientService
+    {
+        private IngredientRepository _repo;
+
+        public IngredientService()
+        {
+            _repo = new IngredientRepository();
+        }
+
+        public List<Ingredient> GetAll()
+        {
+            return _repo.GetAll();
+        }
+
+        public Ingredient GetById(int id)
+        {
+            return _repo.GetById(id);
+        }
+    }
+}
