e99df1ea0970493081f25744ad64d2f2bdafa3a9
JoaoPedroFerreira31 <jpmferreira31@gmail.com>
2020-12-10 18:09:49 +0000

    Update BlockedUser and Admin in WindowsForms

--- WindowsFormsApp/EditUsers.Designer.cs
+++ WindowsFormsApp/EditUsers.Designer.cs
@@ -33,10 +33,7 @@
             this.btnVerTodos = new System.Windows.Forms.Button();
             this.textBox_Id = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.updateBtn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,7 +45,7 @@
             "FirstName",
             "LastName",
             "Email"});
-            this.comboBox1.Location = new System.Drawing.Point(273, 18);
+            this.comboBox1.Location = new System.Drawing.Point(341, 18);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 14;
@@ -60,82 +57,54 @@
             this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView.Location = new System.Drawing.Point(15, 52);
             this.dataGridView.Name = "dataGridView";
-            this.dataGridView.ReadOnly = true;
-            this.dataGridView.Size = new System.Drawing.Size(770, 305);
+            this.dataGridView.Size = new System.Drawing.Size(770, 386);
             this.dataGridView.TabIndex = 13;
             // 
             // btnVerTodos
             // 
             this.btnVerTodos.Location = new System.Drawing.Point(15, 16);
             this.btnVerTodos.Name = "btnVerTodos";
-            this.btnVerTodos.Size = new System.Drawing.Size(128, 23);
+            this.btnVerTodos.Size = new System.Drawing.Size(101, 23);
             this.btnVerTodos.TabIndex = 12;
             this.btnVerTodos.Text = "Ver Todos";
             this.btnVerTodos.UseVisualStyleBackColor = true;
+            this.btnVerTodos.Click += new System.EventHandler(this.btnVerTodos_Click);
             // 
             // textBox_Id
             // 
-            this.textBox_Id.Location = new System.Drawing.Point(400, 19);
+            this.textBox_Id.Location = new System.Drawing.Point(468, 19);
             this.textBox_Id.Name = "textBox_Id";
-            this.textBox_Id.Size = new System.Drawing.Size(385, 20);
+            this.textBox_Id.Size = new System.Drawing.Size(317, 20);
             this.textBox_Id.TabIndex = 11;
+            this.textBox_Id.TextChanged += new System.EventHandler(this.textBox_Id_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label1.Location = new System.Drawing.Point(174, 21);
+            this.label1.Location = new System.Drawing.Point(242, 21);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(94, 13);
             this.label1.TabIndex = 10;
             this.label1.Text = "Filtrar Pesquisa";
             // 
-            // textBox1
+            // updateBtn
             // 
-            this.textBox1.Location = new System.Drawing.Point(109, 370);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(101, 20);
-            this.textBox1.TabIndex = 16;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label2.Location = new System.Drawing.Point(15, 372);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(88, 13);
-            this.label2.TabIndex = 15;
-            this.label2.Text = "Primeiro Nome";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(315, 370);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(101, 20);
-            this.textBox2.TabIndex = 18;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label3.Location = new System.Drawing.Point(231, 373);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(78, 13);
-            this.label3.TabIndex = 17;
-            this.label3.Text = "Ultimo Nome";
+            this.updateBtn.Location = new System.Drawing.Point(128, 16);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(101, 23);
+            this.updateBtn.TabIndex = 15;
+            this.updateBtn.Text = "Update";
+            this.updateBtn.UseVisualStyleBackColor = true;
+            this.updateBtn.Click += new System.EventHandler(this.updateBtn_Click);
             // 
             // EditUsers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.updateBtn);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.dataGridView);
             this.Controls.Add(this.btnVerTodos);
@@ -143,6 +112,7 @@
             this.Controls.Add(this.label1);
             this.Name = "EditUsers";
             this.Text = "EditUsers";
+            this.Load += new System.EventHandler(this.EditUsers_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -156,9 +126,6 @@
         private System.Windows.Forms.Button btnVerTodos;
         private System.Windows.Forms.TextBox textBox_Id;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button updateBtn;
     }
 }
--- WindowsFormsApp/EditUsers.cs
+++ WindowsFormsApp/EditUsers.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using PRE.Model.Model;
+using PRE.Services.Services;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +16,91 @@ namespace WindowsFormsApp
 {
     public partial class EditUsers : Form
     {
+        //Connection String
+        private static string _cs = ConfigurationManager.ConnectionStrings["ReceitasEstrelaresCS"].ConnectionString;
+        SqlConnection connection = new SqlConnection(_cs);
+
         public EditUsers()
         {
             InitializeComponent();
+        }
+
+        private void EditUsers_Load(object sender, EventArgs e)
+        {
+            UserService userService = new UserService();
+
+            List<User> users = userService.GetAll();
+            dataGridView.DataSource = users;
+        }
+
+        private void btnVerTodos_Click(object sender, EventArgs e)
+        {
+            UserService userService = new UserService();
+
+            List<User> users = userService.GetAll();
+            dataGridView.DataSource = users;
+        }
+
+        private void textBox_Id_TextChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "IdUser")
+            {
+                SqlDataAdapter sda = new SqlDataAdapter("SELECT IdUser, FirstName, LastName, Gender, Email, BirthDate, IsAdmin, Blocked FROM Users_tbl WHERE IdUser LIKE '" + textBox_Id.Text + "%'", connection);
+                DataTable dataTable = new DataTable();
+                sda.Fill(dataTable);
+                dataGridView.DataSource = dataTable;
+            }
+            else if (comboBox1.Text == "FirstName")
+            {
+                SqlDataAdapter sda = new SqlDataAdapter("SELECT IdUser, FirstName, LastName, Gender, Email, BirthDate, IsAdmin, Blocked FROM Users_tbl WHERE FirstName LIKE '" + textBox_Id.Text + "%'", connection);
+                DataTable dataTable = new DataTable();
+                sda.Fill(dataTable);
+                dataGridView.DataSource = dataTable;
+            }
+            else if (comboBox1.Text == "LastName")
+            {
+                SqlDataAdapter sda = new SqlDataAdapter("SELECT IdUser, FirstName, LastName, Gender, Email, BirthDate, IsAdmin, Blocked FROM Users_tbl WHERE LastName LIKE '" + textBox_Id.Text + "%'", connection);
+                DataTable dataTable = new DataTable();
+                sda.Fill(dataTable);
+                dataGridView.DataSource = dataTable;
+            }
+            else if (comboBox1.Text == "Email")
+            {
+                SqlDataAdapter sda = new SqlDataAdapter("SELECT IdUser, FirstName, LastName, Gender, Email, BirthDate, IsAdmin, Blocked FROM Users_tbl WHERE Email LIKE '" + textBox_Id.Text + "%'", connection);
+                DataTable dataTable = new DataTable();
+                sda.Fill(dataTable);
+                dataGridView.DataSource = dataTable;
+            }
+        }
+
+        private void updateBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (DataGridViewRow row in dataGridView.Rows) //looping through each records in datagridview
+                {
+                    SqlCommand cmd;
+
+                    //Update query
+                    string query = @"Update Users_tbl Set IsAdmin = @IsAdmin, Blocked = @Blocked  Where IdUser = @IdUser";
+
+                    cmd = new SqlCommand(query, connection);
+                    cmd.Parameters.AddWithValue("@IdUser", row.Cells["IdUser"].Value); //reading Id of a particular record
+                    cmd.Parameters.AddWithValue("@Blocked", SqlDbType.Bit).Value = (Convert.ToBoolean(row.Cells["Blocked"].Value) ? 1 : 0);
+                    cmd.Parameters.AddWithValue("@IsAdmin", SqlDbType.Bit).Value = (Convert.ToBoolean(row.Cells["IsAdmin"].Value) ? 1 : 0);
+
+                    connection.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    connection.Close();
+                }
+                MessageBox.Show("O update foi guardado na tabela.", "Informação");                                        
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message.ToString(), "Error");
+            }
         }
     }
 }
